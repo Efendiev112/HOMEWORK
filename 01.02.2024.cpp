c615f134d6f041f6e528dadaeb377cf1ad284179
Vadim <56439970+Efendiev112@users.noreply.github.com>
2024-02-01 21:53:44 +0300

    Add files via upload

---
+++ 01.02.2024.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+bool linearSearch(int arr[], int size, int key) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == key) {
+            return true;  
+        }
+    }
+     
+}
+
+int main() {
+    
+    setlocale(LC_ALL, "");
+    int array[] = { 8,15,23,54,52,21,37,17,600 };
+    int size = sizeof(array) / sizeof(array[0]);
+    int Key =1;
+    
+
+    if (linearSearch(array, size, Key)) {
+        cout << "Ключ найден в массиве.\n"<<Key;
+    }
+    else {
+        cout << "Ключ не найден в массиве.\n";
+    }
+
+}
