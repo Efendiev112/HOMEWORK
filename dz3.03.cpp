16f8489dc5e5429c3193cd9c840518f3e320f8dc
Vadim <56439970+Efendiev112@users.noreply.github.com>
2024-03-11 00:02:14 +0300

    Update dz3.03.cpp

--- dz3.03.cpp
+++ dz3.03.cpp
@@ -1,20 +1,68 @@
-﻿// ConsoleApplication12.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+void addString(string* &array, string stroka, int size) {
+    string* buf = new string[size + 1];
+    for (int i = 0; i < size; i++)
+    {
+        buf[i] = array[i];
+    }
+    buf[size] = stroka;
+    delete[] array;
+    array = buf;
+}
+void readfile(string path, string*& array, int& size) {
+    string* stringArray = new string[0];
+    ifstream in(path);
+    if (in.is_open()) {
+        string str;
+        while (getline(in, str))
+        {
+            addString(stringArray, str, size++);
+        }
+    }
+   
+    delete[] array;
+    array = stringArray;
+}
+void showArray(string* array, int size) {
+    for (int i = 0; i < size; i++)
+    {
+        cout << array[i] << endl;
+    }
+}
+void copy(string path) {
+    string* stringArray = new string[0];
+    int size = 0;
+    readfile(path, stringArray, size);
 
-#include <iostream>
 
+
+    string copyPath;
+    for (int i = 0; i < path.length(); i++) {
+        if (path[i] == '.') copyPath += "(COPY)";
+        copyPath += path[i];
+    }
+
+
+
+    ofstream out(copyPath);
+    if (out.is_open()) {
+        for (int i = 0; i < size; i++)
+        {
+            out << stringArray[i] << endl;
+        }
+    }
+    out.close();
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "rus");
+    //int arraySize = 0;
+    //string* stringArray = new string[0];
+    //readfile("hello.txt", stringArray, arraySize);
+    //showArray(stringArray, arraySize);
+    copy("hello.txt");
+
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
